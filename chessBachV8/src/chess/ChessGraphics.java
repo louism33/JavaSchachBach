b7904ea93b52c32ed13fce8e3e24d7bba3c0c003
Louis <louis.mackenziesmith@googlemail.com>
2018-09-20 21:34:12 +0200

    refactor

--- chessBachV8/src/chess/ChessGraphics.java
+++ chessBachV8/src/chess/ChessGraphics.java
@@ -18,90 +18,116 @@ public class ChessGraphics {
     private final JPanel gui = new JPanel(new BorderLayout(3, 3));
     private JButton[][] chessBoardSquares = new JButton[8][8];
     private JPanel chessBoard;
-    private final JLabel message = new JLabel(
-            "Louis' Chess");
+    private final JLabel message = new JLabel("Louis' Chess");
     private static final String COLS = "ABCDEFGH";
-
     private Engine engine;
-
+    private GraphicsImageManager gim;
     private ChessBoard board;
-
-    private BufferedImage whitePawn, whiteKnight, whiteBishop, whiteRook, whiteQueen, whiteKing;
-    private BufferedImage blackPawn, blackKnight, blackBishop, blackRook, blackQueen, blackKing;
-
+    private List<ChessBoard> pastBoards, futureBoards;
     private Color black, white;
-
     private List<Integer> takenPieces;
 
     ChessGraphics(ChessBoard board, Engine engine) {
+        initializeGraphicsHelper();
+        pastBoards = new ArrayList<>();
+        futureBoards = new ArrayList<>();
         this.board = board;
         this.possibleMove = new PossibleMove(board);
         this.engine = engine;
         takenPieces = new ArrayList<>();
-        loadPics();
-        pickColors();
         initializeGui();
     }
 
-    void pickColors(){
-        boolean style1 = false;
-        boolean style2 = false;
-        boolean style3 = true;
-
-        if (style1) {
-            black = Color.black;
-            white = Color.white;
-        } else if (style2) {
-            black = new Color(218, 165, 32);
-            white = new Color(222, 184, 135);
-        } else if (style3) {
-            black = new Color(139, 69, 19);
-            white = new Color(205, 133, 63);
-        }
+    void initializeGraphicsHelper(){
+        this.gim = new GraphicsImageManager(this);
+        black = gim.getBlack();
+        white = gim.getWhite();
     }
 
-
-
     void initializeGui() {
-
-        // set up the main GUI
         gui.setBorder(new EmptyBorder(5, 5, 5, 5));
         JToolBar tools = new JToolBar();
         tools.setFloatable(false);
         gui.add(tools, BorderLayout.PAGE_START);
 
-        JButton newB = new JButton("posmove");
-        newB.addActionListener(new ActionListener() {
+        JButton list = new JButton("List Previous Boards");
+        list.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                System.out.println("possibleMove is: " + possibleMove);
+                int numberOfPastBoards = pastBoards.size();
+                if (numberOfPastBoards > 0) {
+                    for (int n = 0; n < numberOfPastBoards; n++){
+                        System.out.println(pastBoards.get(n));
+                    }
+                } else {
+                    System.out.println("No past boards.");
+                }
             }
         });
+        tools.add(list);
 
-        tools.add(newB);
-
-        JButton spotty = new JButton("boards");
-        spotty.addActionListener(new ActionListener() {
+        JButton prevBoard = new JButton("Prev Board");
+        prevBoard.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                System.out.println(board);
+                int numberOfBoards = pastBoards.size();
+                if (numberOfBoards > 0){
+                    ChessBoard oldBoard = pastBoards.get(numberOfBoards - 1);
+                    pastBoards.remove(numberOfBoards - 1);
+
+                    System.out.println(board.turn);
+
+                    futureBoards.add(board);
+                    setBoard(oldBoard);
+                    System.out.println(board.turn);
+                } else {
+                    System.out.println("No past boards.");
+                }
             }
         });
-        tools.add(spotty);
+        tools.add(prevBoard);
+
+
+
+        JButton nextBoard = new JButton("Next Board");
+        nextBoard.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                int numberOfBoards = futureBoards.size();
+                if (numberOfBoards > 0){
+                    ChessBoard futureBoard = futureBoards.get(numberOfBoards - 1);
+                    futureBoards.remove(numberOfBoards - 1);
+
+                    System.out.println(futureBoards);
+
+                    pastBoards.add(board);
+                    setBoard(futureBoard);
+                }
+                else {
+                    System.out.println("No next boards.");
+                }
+            }
+        });
+        tools.add(nextBoard);
 
         JButton takenPiece = new JButton("TakenPieces");
         takenPiece.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                System.out.println(takenPieces);
+                int numberOfBoards = takenPieces.size();
+                if (numberOfBoards > 0) {
+                    System.out.println(takenPieces);
+                }
+                else {
+                    System.out.println("No pieces have been taken.");
+                }
             }
         });
         tools.add(takenPiece);
         tools.addSeparator();
 
-
-        JButton aiMove = new JButton("AI Move");
-
+        JButton aiMove =
+                new JButton("AI Move");
         aiMove.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
@@ -109,16 +135,17 @@ public class ChessGraphics {
                 dealWithMove(m);
             }
         });
-        tools.add(aiMove); // TODO - add functionality!
+        tools.add(aiMove);
         tools.addSeparator();
         tools.add(message);
 
-        gui.add(new JLabel("?"), BorderLayout.LINE_START);
 
+
+
+        gui.add(new JLabel("?"), BorderLayout.LINE_START);
         chessBoard = new JPanel(new GridLayout(0, 9));
         chessBoard.setBorder(new LineBorder(Color.BLACK));
         gui.add(chessBoard);
-
         Insets buttonMargin = new Insets(0,0,0,0);
         for (int ii = 0; ii < chessBoardSquares.length; ii++) {
             for (int jj = 0; jj < chessBoardSquares[ii].length; jj++) {
@@ -127,7 +154,7 @@ public class ChessGraphics {
 
                 ChessBoard.SquareDesc sq = board.getSquare((jj), (7-ii));
 
-                ImageIcon imageIcon = iconFinder(sq);
+                ImageIcon imageIcon = gim.iconFinder(sq);
 
                 b.setIcon(imageIcon);
                 if ((jj + ii) % 2 == 0) {
@@ -256,7 +283,10 @@ public class ChessGraphics {
             System.out.println(takenPieces);
         }
 
+        ChessBoard recordBoard = new ChessBoard(board);
+        pastBoards.add(recordBoard);
         board.makeMove(move);
+        futureBoards = new ArrayList<>();
         setBoard(board);
 
     }
@@ -269,108 +299,118 @@ public class ChessGraphics {
         int desty = move.desty;
 
         ChessBoard.SquareDesc readDestFromBoard = board.getSquare(destx, desty);
-        Icon icon = iconFinder(readDestFromBoard);
+        Icon icon = gim.iconFinder(readDestFromBoard);
         chessBoardSquares[srcx][7-srcy].setIcon(null);
         chessBoardSquares[destx][7-desty].setIcon(null);
         chessBoardSquares[destx][7-desty].setIcon(icon);
 
-        if (readDestFromBoard.type == PAWN){ // dealing with enpassant like a boss
+        if (readDestFromBoard.type == PAWN){
 
             ChessBoard.SquareDesc destPlusOne = board.getSquare(destx, desty+1);
-            Icon icon1 = iconFinder(destPlusOne);
+            Icon icon1 = gim.iconFinder(destPlusOne);
 
             ChessBoard.SquareDesc destMinusOne = board.getSquare(destx, desty-1);
-            Icon icon2 = iconFinder(destMinusOne);
+            Icon icon2 = gim.iconFinder(destMinusOne);
 
             chessBoardSquares[destx][7-destPlusOne.y].setIcon(icon1);
             chessBoardSquares[destx][7-destMinusOne.y].setIcon(icon2);
 
-        } else if (readDestFromBoard.type == KING &&
-                (readDestFromBoard.x == 1 || readDestFromBoard.x == 6)){ // castling
-            System.out.println("woo");
+        } else if (readDestFromBoard.type == KING && readDestFromBoard.x == 2){
 
-            ChessBoard.SquareDesc destLeftOne = board.getSquare(destx-1, desty);
-            Icon icon1 = iconFinder(destLeftOne);
+            ChessBoard.SquareDesc destLeftOne = board.getSquare(destx-2, desty);
+            Icon icon1 = gim.iconFinder(destLeftOne);
 
             ChessBoard.SquareDesc destRightOne = board.getSquare(destx+1, desty);
-            Icon icon2 = iconFinder(destRightOne);
+            Icon icon2 = gim.iconFinder(destRightOne);
+
+            chessBoardSquares[destx-2][7-desty].setIcon(icon1);
+            chessBoardSquares[destx+1][7-desty].setIcon(icon2);
+
+        }   else if (readDestFromBoard.type == KING && readDestFromBoard.x == 6){
+
+            ChessBoard.SquareDesc destLeftOne = board.getSquare(destx-1, desty);
+            Icon icon1 = gim.iconFinder(destLeftOne);
+
+            ChessBoard.SquareDesc destRightOne = board.getSquare(destx+1, desty);
+            Icon icon2 = gim.iconFinder(destRightOne);
 
             chessBoardSquares[destx-1][7-desty].setIcon(icon1);
             chessBoardSquares[destx+1][7-desty].setIcon(icon2);
+
         }
     }
 
 
-    private ImageIcon iconFinder(ChessBoard.SquareDesc sq){
-        ImageIcon imageIcon = new ImageIcon();
-        switch (sq.type) {
-            case PAWN:
-                if (sq.color == BLACK) {
-                    imageIcon = new ImageIcon(blackPawn);
-                } else {
-                    imageIcon = new ImageIcon(whitePawn);
-                }
-                break;
-            case KNIGHT:
-                if (sq.color == BLACK) {
-                    imageIcon = new ImageIcon(blackKnight);
-                } else {
-                    imageIcon = new ImageIcon(whiteKnight);
-                }
-                break;
-            case BISHOP:
-                if (sq.color == BLACK) {
-                    imageIcon = new ImageIcon(blackBishop);
-                } else {
-                    imageIcon = new ImageIcon(whiteBishop);
-                }
-                break;
-            case ROOK:
-                if (sq.color == BLACK) {
-                    imageIcon = new ImageIcon(blackRook);
-                } else {
-                    imageIcon = new ImageIcon(whiteRook);
-                }
-                break;
-            case QUEEN:
-                if (sq.color == BLACK) {
-                    imageIcon = new ImageIcon(blackQueen);
-                } else {
-                    imageIcon = new ImageIcon(whiteQueen);
-                }
-                break;
-            case KING:
-                if (sq.color == BLACK) {
-                    imageIcon = new ImageIcon(blackKing);
-                } else {
-                    imageIcon = new ImageIcon(whiteKing);
-                }
-                break;
-        }
-        return imageIcon;
-    }
+//    private ImageIcon iconFinder(ChessBoard.SquareDesc sq){
+//        ImageIcon imageIcon = new ImageIcon();
+//        switch (sq.type) {
+//            case PAWN:
+//                if (sq.color == BLACK) {
+//                    imageIcon = new ImageIcon(blackPawn);
+//                } else {
+//                    imageIcon = new ImageIcon(whitePawn);
+//                }
+//                break;
+//            case KNIGHT:
+//                if (sq.color == BLACK) {
+//                    imageIcon = new ImageIcon(blackKnight);
+//                } else {
+//                    imageIcon = new ImageIcon(whiteKnight);
+//                }
+//                break;
+//            case BISHOP:
+//                if (sq.color == BLACK) {
+//                    imageIcon = new ImageIcon(blackBishop);
+//                } else {
+//                    imageIcon = new ImageIcon(whiteBishop);
+//                }
+//                break;
+//            case ROOK:
+//                if (sq.color == BLACK) {
+//                    imageIcon = new ImageIcon(blackRook);
+//                } else {
+//                    imageIcon = new ImageIcon(whiteRook);
+//                }
+//                break;
+//            case QUEEN:
+//                if (sq.color == BLACK) {
+//                    imageIcon = new ImageIcon(blackQueen);
+//                } else {
+//                    imageIcon = new ImageIcon(whiteQueen);
+//                }
+//                break;
+//            case KING:
+//                if (sq.color == BLACK) {
+//                    imageIcon = new ImageIcon(blackKing);
+//                } else {
+//                    imageIcon = new ImageIcon(whiteKing);
+//                }
+//                break;
+//        }
+//        return imageIcon;
+//    }
 
-    void loadPics(){
-        try {
-            blackPawn = ImageIO.read(getClass().getResource("/res/blackPawn.png"));
-            blackKnight = ImageIO.read(getClass().getResource("/res/blackKnight.png"));
-            blackBishop = ImageIO.read(getClass().getResource("/res/blackBishop.png"));
-            blackRook = ImageIO.read(getClass().getResource("/res/blackRook.png"));
-            blackQueen = ImageIO.read(getClass().getResource("/res/blackQueen.png"));
-            blackKing = ImageIO.read(getClass().getResource("/res/blackKing.png"));
-
-            whitePawn = ImageIO.read(getClass().getResource("/res/whitePawn.png"));
-            whiteKnight = ImageIO.read(getClass().getResource("/res/whiteKnight.png"));
-            whiteBishop = ImageIO.read(getClass().getResource("/res/whiteBishop.png"));
-            whiteRook = ImageIO.read(getClass().getResource("/res/whiteRook.png"));
-            whiteQueen = ImageIO.read(getClass().getResource("/res/whiteQueen.png"));
-            whiteKing = ImageIO.read(getClass().getResource("/res/whiteKing.png"));
-
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
-    }
+//    void loadPics(){
+//        try {
+//            blackPawn = ImageIO.read(getClass().getResource("/res/blackPawn.png"));
+//            blackKnight = ImageIO.read(getClass().getResource("/res/blackKnight.png"));
+//            blackBishop = ImageIO.read(getClass().getResource("/res/blackBishop.png"));
+//            blackRook = ImageIO.read(getClass().getResource("/res/blackRook.png"));
+//            blackQueen = ImageIO.read(getClass().getResource("/res/blackQueen.png"));
+//            blackKing = ImageIO.read(getClass().getResource("/res/blackKing.png"));
+//
+//            whitePawn = ImageIO.read(getClass().getResource("/res/whitePawn.png"));
+//            whiteKnight = ImageIO.read(getClass().getResource("/res/whiteKnight.png"));
+//            whiteBishop = ImageIO.read(getClass().getResource("/res/whiteBishop.png"));
+//            whiteRook = ImageIO.read(getClass().getResource("/res/whiteRook.png"));
+//            whiteQueen = ImageIO.read(getClass().getResource("/res/whiteQueen.png"));
+//            whiteKing = ImageIO.read(getClass().getResource("/res/whiteKing.png"));
+//
+//        }
+//        catch (IOException e){
+//            e.printStackTrace();
+//        }
+//    }
 
     public final JComponent getChessBoard() {
         return chessBoard;
